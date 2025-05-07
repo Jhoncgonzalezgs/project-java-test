403d8b24e630d35d185c8fe7bdf5ad4725870c5b
Jhoncgonzalezgs <jhonc.gonzalezg@gmail.com>
2025-05-06 19:47:05 -0500

    Trabajar con Numeros de Punto Flotante - JhonGz

---
+++ src/FloatDemo.java
@@ -0,0 +1,13 @@
+// --------------------------------------- Numeros de Punto Flotante ---------------------------------------
+public class FloatDemo{ 
+    public static void main(String[] args) {
+        double PI = 3.14159;
+        System.out.println("El valor de PI aproximadamente es: " + PI);
+
+        double Radius = 5.0;
+        double Area = PI * Radius * Radius;
+        System.out.println("El Radio de las circunferencia es: " + Radius);
+        System.out.println("El Area del circulo es: " + Area);
+                
+    }
+}
