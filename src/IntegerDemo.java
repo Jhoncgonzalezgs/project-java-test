79ecb5bee7ef37d8dfa2603f4d37ba617270ebf1
Jhoncgonzalezgs <jhonc.gonzalezg@gmail.com>
2025-05-06 19:43:47 -0500

    Operaciones Aritmeticas Basicas - JhonGz

--- src/IntegerDemo.java
+++ src/IntegerDemo.java
@@ -1,8 +1,21 @@
 // --------------------------------------- Trabajar Con Numero Enteros ---------------------------------------
 public class IntegerDemo {
     public static void main(String[] args){
-        int Numero = 42;
-        System.out.println("El Numero es: " + Numero);
+        //int Numero = 42;
+        //System.out.println("El Numero es: " + Numero);
+// --------------------------------------- Operaciones Aritmeticas Basicas ---------------------------------------
+        int Numero1 = 10;
+        int Numero2 = 5;
+        int suma = Numero1 + Numero2;
+        int resta = Numero1 - Numero2;
+        int multiplicacion = Numero1 * Numero2;
+        int division = Numero1 / Numero2;
 
+        System.out.println("El Numero 1 es: " + Numero1);
+        System.out.println("El Numero 2 es: " + Numero2);
+        System.out.println("La suma es igual a: " + suma);
+        System.out.println("La resta es igual a: " + resta);
+        System.out.println("La multiplicacion es igual a: " + multiplicacion);
+        System.out.println("La division es igual a: " + division);
     }
 }
