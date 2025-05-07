1ad8f4473ed727eb93b50e737e8de6ba3c799be5
Jhoncgonzalezgs <jhonc.gonzalezg@gmail.com>
2025-05-06 19:40:31 -0500

    Trabajar con Numeros Enteros - JhonGz

---
+++ src/IntegerDemo.java
@@ -0,0 +1,8 @@
+// --------------------------------------- Trabajar Con Numero Enteros ---------------------------------------
+public class IntegerDemo {
+    public static void main(String[] args){
+        int Numero = 42;
+        System.out.println("El Numero es: " + Numero);
+
+    }
+}
